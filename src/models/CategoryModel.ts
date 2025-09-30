742ee17f24adb1e589e64a52c499dd94915824f4
marianyarj <marianyarj@gmail.com>
2025-09-30 09:48:28 +0200

    feat: create CategoryModel (#20)

---
+++ src/models/CategoryModel.ts
@@ -0,0 +1,39 @@
+import {
+    Table,
+    Column,
+    Model,
+    PrimaryKey,
+    AutoIncrement,
+    AllowNull,
+    DataType,
+    HasMany,
+} from "sequelize-typescript";
+import type { CategoryAttributes, CategoryCreationAttributes } from "../types/category.js";
+import type { PostModel } from "./PostModel.js";
+import { PostModel as PostModelClass } from "./PostModel.js";
+import { SubcategoryModel } from "./SubcategoryModel.js";
+import { SubcategoryModel as SubcategoryModelClass } from "./SubcategoryModel.js";
+
+@Table({
+    tableName: "categories"
+})
+
+export class CategoryModel extends Model<CategoryAttributes, CategoryCreationAttributes> {
+    @PrimaryKey
+    @AutoIncrement
+    @Column
+    id!: number;
+
+    @AllowNull(false)
+    @Column({
+        unique: true,
+        type: DataType.ENUM('botanical', 'astronomy'),
+    })
+    name!: string;
+
+    @HasMany(() => PostModelClass)
+    posts!: PostModel[];
+
+    @HasMany(() => SubcategoryModelClass)
+    subcategories!: SubcategoryModel[];
+}
