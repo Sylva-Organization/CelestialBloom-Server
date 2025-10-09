528c2135361e8460839725d009f3a04452a28566
priscelis <prisceliscodrington@gmail.com>
2025-10-09 10:24:19 +0200

    adding validations to UserRoutes.ts (#12)

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -5,18 +5,21 @@ import {
     updateUser,
     deleteUser
 }from '../controllers/UsersController.js';
+import { validateDeleteUser, validateGetAllUsers, validateUpdateUser, validateUserId } from '../Validations/UserValidations.js';
 //import * as PostsController from '../controllers/PostsController.js'
-// import * as AuthController from
+//import { validateUpdateUser,  } from '../Validations/UserValidations.js';
 
 
 const UserRouter = Router();
 
+
+
 // Users
-UserRouter.get('/', getAllUsers);
+UserRouter.get('/', validateGetAllUsers, getAllUsers );
 //UserRouter.post('/', )
-UserRouter.put('/:id', updateUser);
-UserRouter.delete('/:id', deleteUser);
-UserRouter.get('/:id', getOneUser);
+UserRouter.put('/:id', validateUpdateUser, updateUser );
+UserRouter.delete('/:id', validateDeleteUser, deleteUser );
+UserRouter.get('/:id', validateUserId, getOneUser );
 
 export default UserRouter;
 
