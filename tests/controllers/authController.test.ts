abd006c757e9157716920300bd0065bb882a85d2
marianyarj <marianyarj@gmail.com>
2025-10-09 11:44:17 +0200

    test: add unit test for AuthController register 400 case (#11)

--- tests/controllers/authController.test.ts
+++ tests/controllers/authController.test.ts
@@ -27,4 +27,22 @@ describe('AuthController', () => {
         await teardownTestDB();
     });
 
+    describe('register', () => {
+        it.each([
+            [{ first_name: '', last_name: 'Doe', email: 'test@test.com', password: 'test123', nick_name: 'teste' }],
+            [{ first_name: 'Test', last_name: '', email: 'test@test.com', password: 'test123', nick_name: 'teste' }],
+            [{ first_name: 'Test', last_name: 'Doe', email: '', password: 'test123', nick_name: 'teste' }],
+            [{ first_name: 'Test', last_name: 'Doe', email: 'test@test.com', password: '', nick_name: 'teste' }],
+            [{ first_name: 'Test', last_name: 'Doe', email: 'test@test.com', password: 'test123', nick_name: '' }]
+        ])('register >> should respond 400 if any required field is missing', async (partBody) => {
+            const req = { body: partBody } as unknown as Request;
+            const res = createMockResponse();
+
+            await register(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: 'All fields are required' });
+        });
+
+    });
 });
