c4c5e2da456a1001421a669a28321596e8fafe35
priscelis <prisceliscodrington@gmail.com>
2025-10-10 09:09:14 +0200

    adding userrouutes, postroutes and authroutes (#35)

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -6,8 +6,6 @@ import {
     deleteUser
 }from '../controllers/UsersController.js';
 import { validateDeleteUser, validateGetAllUsers, validateUpdateUser, validateUserId } from '../Validations/UserValidations.js';
-//import * as PostsController from '../controllers/PostsController.js'
-//import { validateUpdateUser,  } from '../Validations/UserValidations.js';
 
 
 const UserRouter = Router();
@@ -16,20 +14,11 @@ const UserRouter = Router();
 
 // Users
 UserRouter.get('/', validateGetAllUsers, getAllUsers );
-//UserRouter.post('/', )
 UserRouter.put('/:id', validateUpdateUser, updateUser );
 UserRouter.delete('/:id', validateDeleteUser, deleteUser );
 UserRouter.get('/:id', validateUserId, getOneUser );
 
 export default UserRouter;
 
-// Posts
-// router.get('/posts', PostsController);
-// router.post('/posts', PosgetAllUsers,
-
-
-// Auth
-// router.post('/auth/register', AuthController.register);
-// router.post('/auth/login', AuthController.login);
 
 
